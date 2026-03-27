8d1c9ac72f74c89c076adba969de8c59205660a1
Kuralbek798 <agubaev_kuralbek@mail.ru>
2026-03-27 09:38:01 +0500

    arrow function in state change - useState.

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,28 @@ const {array} = functions;
 array
 function App() {
   const[value, setValue] = useState('');
+  const[count, setCount] = useState(0)
+ 
+  function increaseCount(){
+   
+  setCount((count) => count +1);
+   
+  
+  }
+  
   return (
     <div>
       <h1>My Hacker Stories</h1>
       <Search value ={value} setValue = {setValue} />
       <hr />
       <List array = {array} />
+      <button type='button' onClick ={increaseCount}>
+       Increase
+      </button>
+      <p>{'Count' +  count}</p>
     </div>
   );
+  
 }
 
 export default App;
