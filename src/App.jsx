2ed09e09ba92fa6f171d3e7f8e61bc16b7fd11d9
Kuralbek798 <agubaev_kuralbek@mail.ru>
2026-04-01 14:42:47 +0500

    useEffect() and localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,30 @@ import { useState } from "react";
 import List from "./components/List";
 import Search from "./components/Search";
 import functions from "./functions";
+import { useEffect } from "react";
 
-
-const {array} = functions;
-array
+const { array } = functions;
+array;
 function App() {
-
-  const[searchTerm, setSearchTerm] = useState('React'); 
-  const handleSearch = (event) => setSearchTerm(event.target.value);
-
-
+  const [searchTerm, setSearchTerm] = useState(
+    localStorage.getItem("search") || "React",
+  );
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+    localStorage.setItem("search", event.target.value);
+  };
+  useEffect(() => {
+    localStorage.setItem("search", searchTerm);
+  }, [searchTerm]);
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <Search Search search ={searchTerm} onSearch = {handleSearch} />
+      <Search Search search={searchTerm} onSearch={handleSearch} />
       <hr />
-      <List array = {array} />
-     
+      <List array={array} />
     </div>
   );
-  
 }
 
 export default App;
